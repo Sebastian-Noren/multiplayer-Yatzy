29a677b6f3f774443606768f3a53d86e3d53e261
Sebastian Norén <s.noren89@gmail.com>
2020-05-06 07:20:50 +0200

    More updates in game rules, colorfields etc. Game GUI

--- app/src/main/java/software/engineering/yatzy/game/GameRules.java
+++ app/src/main/java/software/engineering/yatzy/game/GameRules.java
@@ -5,29 +5,28 @@ import java.util.Arrays;
 public class GameRules {
 
     //Stor stege
-    public boolean largeStraight(int[] i) {
+    public boolean largeStraight(int[] dice) {
         boolean isStraight = false;
-        Arrays.sort(i);
 
-        if (((i[0] == 2) &&
-                (i[1] == 3) &&
-                (i[2] == 4) &&
-                (i[3] == 5) &&
-                (i[4] == 6))) {
+        if (((dice[0] == 2) &&
+                (dice[1] == 3) &&
+                (dice[2] == 4) &&
+                (dice[3] == 5) &&
+                (dice[4] == 6))) {
             isStraight = true;
         }
         return isStraight;
     }
-    // leten stege
-    public boolean smallStraight(int[] i) {
-        boolean isStraight = false;
-        Arrays.sort(i);
 
-        if (((i[0] == 1) &&
-                (i[1] == 2) &&
-                (i[2] == 3) &&
-                (i[3] == 4) &&
-                (i[4] == 5))) {
+    // liten stege
+    public boolean smallStraight(int[] dice) {
+        boolean isStraight = false;
+
+        if (((dice[0] == 1) &&
+                (dice[1] == 2) &&
+                (dice[2] == 3) &&
+                (dice[3] == 4) &&
+                (dice[4] == 5))) {
             isStraight = true;
         }
         return isStraight;
@@ -36,8 +35,6 @@ public class GameRules {
     //KÅK
     public boolean fullHouse(int[] dice) {
         boolean isFullHouse = false;
-
-        Arrays.sort(dice);
         if ((((dice[0] == dice[1]) && (dice[1] == dice[2])) && (dice[3] == dice[4])
                 && (dice[2] != dice[3])) ||
                 ((dice[0] == dice[1]) && ((dice[2] == dice[3]) && (dice[3] == dice[4]))
@@ -46,16 +43,31 @@ public class GameRules {
         }
         return isFullHouse;
     }
+
+    public boolean yatzy( int[] dice ) {
+        boolean yatzy = false;
+        for( int i = 1; i <= 6; i++ ) {
+            int count = 0;
+            for( int j = 0; j < 5; j++ ) {
+                if( dice[j] == i ) {
+                    count++;
+                }
+                yatzy = (count > 4);
+            }
+        }
+        return yatzy;
+    }
+
     // triss
-    public boolean threeOfAKind(int[] myDice) {
+    public boolean threeOfAKind(int[] dice) {
         boolean threeDice = false;
         for (int i = 1; i <= 6; i++) {
             int count = 0;
             for (int j = 0; j < 5; j++) {
-                if (myDice[j] == i) {
+                if (dice[j] == i) {
                     count++;
                 }
-                threeDice = (count > 2) && (count < 4);
+                threeDice = (count > 2);
             }
         }
         // System.out.println(count);
@@ -63,32 +75,51 @@ public class GameRules {
     }
 
     // fyrpar
-    public boolean fourOfAKind(int[] myDice) {
+    public boolean fourOfAKind(int[] dice) {
         boolean fourDice = false;
         for (int i = 1; i <= 6; i++) {
             int count = 0;
             for (int j = 0; j < 5; j++) {
-                if (myDice[j] == i) {
+                if (dice[j] == i) {
                     count++;
                 }
-                fourDice = count > 3 && count < 5;
+                fourDice = (count > 3);
             }
         }
         return fourDice;
     }
 
-    private static boolean onePair(int[] myDice) {
+    public boolean onePair(int[] dice) {
         boolean isOnePar = false;
         for (int i = 1; i <= 6; i++) {
             int count = 0;
             for (int j = 0; j < 5; j++) {
-                if (myDice[j] == i) {
+                if (dice[j] == i) {
                     count++;
                 }
-                isOnePar = (count > 1) && (count < 3);
+                isOnePar = (count > 1);
             }
         }
         return isOnePar;
+    }
+
+    public boolean twoPair(int[] dice) {
+        boolean isTwoPair = false;
+        if (((dice[0] == dice[1]) && (dice[2] == dice[3])) ||
+                ((dice[1] == dice[2]) && (dice[3] == dice[4]))
+        ) {
+            isTwoPair = true;
+        }
+        return isTwoPair;
+    }
+
+    public boolean singelSide(int diceSide, int[] dice) {
+        for (int die : dice) {
+            if (die == diceSide) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
