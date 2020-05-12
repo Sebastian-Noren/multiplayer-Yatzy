267f65950bc30654dc2b9decdfaa5cf663285fb7
Sebastian Norén <s.noren89@gmail.com>
2020-05-12 08:25:42 +0200

    Final_beta_version

--- app/src/main/java/software/engineering/yatzy/testing/localDatabase/DatabaseOpenHelper.java
+++ app/src/main/java/software/engineering/yatzy/testing/localDatabase/DatabaseOpenHelper.java
@@ -5,7 +5,7 @@ package software.engineering.yatzy.testing.localDatabase;
         import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 public class DatabaseOpenHelper extends SQLiteAssetHelper {
-    private static final String DATABASE_NAME = "mockDatabase.db";
+    private static final String DATABASE_NAME = "databaseName.fileextension";
     private static final int DATABASE_VERSION = 1;
 
     public DatabaseOpenHelper(Context context){
